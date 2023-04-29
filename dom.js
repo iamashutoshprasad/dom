8af5ecc37f17a5098e0ca6e87b566b933596da05
Ashu <ashuprasad1227@gmail.com>
2023-04-29 11:49:42 +0530

    scommit4

---
+++ dom.js
@@ -0,0 +1,5 @@
+var header=document.getElementById("main-header");
+header.style.borderBottom='solid 3px #000'; 
+var secheader=document.getElementById("title2");
+secheader.style.color='green';
+secheader.style.font='bold';
